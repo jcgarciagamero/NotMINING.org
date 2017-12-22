6a4b5fec308a5b07ada7ef102098fef9f1e4df22
disadamsdsdnts <33907227+disadamsdsdnts@users.noreply.github.com>
2017-12-22 08:23:50 +0100

    Add active & passive mode
    When loads a site, active mode is executed (isMining.js): If there is a trace, it alerts to the user.
    
    Passive mode (isMining_click.js): User can click in the icon to see if there is or there is not a trace of miners.

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-   chrome.tabs.executeScript(null, {file: "isMining.js"});
+	chrome.tabs.executeScript(null, {file: "isMining_click.js"});
 });
