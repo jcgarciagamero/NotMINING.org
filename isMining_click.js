404b83c9dfc8627e9700d6ff4dae8958d9f0c1e6
disadamsdsdnts <33907227+disadamsdsdnts@users.noreply.github.com>
2018-01-02 15:10:39 +0100

    Updated to v1.6.8
    International update: Now in japanese, english and spanish.

--- isMining_click.js
+++ isMining_click.js
@@ -4,17 +4,36 @@ function isMining() {
 
     var array = [ "authedmine.com/media/miner.html", "coin-have.com", "coinblind.com", "coinHive.", "CoinHive.Anonymous", "coinhive.min.js", "CoinHive.User", "coinimp.com", "CoinNebula.Instance", "CRLT.Anonymous", "CRLT.User", "miner.start", "minero.pw", "poolWallet", "simple-monero-miner-coin", ".clod.pw/", ".coinimp.com/scripts/", ".cryptonoter.com/processor.js", ".g-content.bid/", ".hallaert.online/", ".hashing.win/scripts/", ".hk.rs/", ".kjli.fi/", ".monerise.com/core/", ".nullrefexcep.com/", ".pema.cl/", ".ron.si/", ".rove.cl/", ".sparechange.io/", ".sparechange.io/static/sparechange.js", ".static-cnt.bid/", ".statistic.date/", ".webminerpool.com/", "185.14.28.10/", "185.209.23.219/", "ad-miner.com/lib", "authedmine.com/media/miner.html", "cieh.mx/", "cloudcoins.co/javascript", "coin-have.com/c/", "coin-hive.com/captcha", "coin-hive.com/proxy", "coinblind.com/lib", "coinerra.com/lib/", "coinhive-manager.com/ch-manager.js", "coinhive-miner", "CoinHive.Anonymous", "coinhive.min.js", "CoinHive.User", "coinlab.biz/lib", "CoinNebula.Instance", "CRLT.Anonymous", "CRLT.User", "crypto-loot.com/lib", "crypto-loot.com/proxy", "d-ns.ga", "d3iz6lralvg77g.cloudfront.net", "harvest.surge.sh/h.js", "https://coin-hive.com/lib/", "https://load.jsecoin.com", "inwemo.com/inwemo.min.js", "js/coinhive.min.js?v2", "jsecoin.com/", "load.jsecoin.com/", "minemytraffic.com/lib", "miner.start", "minero.pw/miner.min.js", "minr.pw/inject", "papoto.com/lib/", "poolWallet", "ppoi.org/lib", "rocks.io/assets/rocks.min.js", "simple-monero-miner-coin", "var miner = new Client", "webmine.cz/miner", "wp-monero-miner", "hashing.win", "coin-have.com/", "baiduccdn1.com/js/" ];
     
+    var powvideo_trick = ["povwideo.net", "location.pathname.replace", "/preview-", "/embed-"];
+    var powvideo_current = 0;
+
     var detecciones = 0;
+
+    var codigoCompleto = document.body.innerHTML;
+
+    for (pwnthis in powvideo_trick){
+        var comprobacion = codigoCompleto.indexOf(powvideo_trick[pwnthis]);
+
+        if (comprobacion >= 0){
+            powvideo_current++;
+        }
+    }
+
+    if (powvideo_current >= 2){
+        detecciones++;
+    }
 
     for (var actualMiner in array) {
         for (var actualScript in codigoWeb){
             var codigoInner = "" + codigoWeb[actualScript].innerHTML;
             var codigoOuter = "" + codigoWeb[actualScript].outerHTML;
 
-            var buscadorInner = codigoInner.indexOf(array[actualMiner]);
-            var buscadorOuter = codigoOuter.indexOf(array[actualMiner]);
-                
-            if ((buscadorInner != -1) || (buscadorOuter != -1)) {
+            if (codigoOuter.indexOf("https://www.google") == -1){
+                var buscadorInner = codigoInner.indexOf(array[actualMiner]);
+                var buscadorOuter = codigoOuter.indexOf(array[actualMiner]);
+            }  
+
+            if ((buscadorInner > -1) || (buscadorOuter > -1)) {
                 detecciones++;
             }
         }
@@ -39,9 +58,13 @@ function isMining() {
     }
 
     if (detecciones > 0) {
-        alert("EN:\nThere is a trace of cryptominers in this webpage. Maybe this site is using your CPU power without your consent.\n\nES:\nSe ha encontrado rastros de código de minado de criptomoneda. Esta página puede estar utilizando tu CPU sin permiso previo.");
+        var msg = chrome.i18n.getMessage("positive");
+
+        alert(msg);
     } else if (detecciones == 0){
-        alert("EN:\nThere is not trace of cryptominers in this webpage. You are safe in this place.\n\nES:\nNo se ha encontrado rastro de código de minado de criptomoneda. Puedes navegar seguro en esta web.");
+        var msg = chrome.i18n.getMessage("negative");
+
+        alert(msg);    
     }
 }
 
